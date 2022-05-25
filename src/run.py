a911e027f90eaea9a3eeea3ee4e709feaae67462
Markus Schneider <mschneider@linkdot.org>
2022-05-25 11:51:25 +0200

    using different cropping

--- src/run.py
+++ src/run.py
@@ -1,76 +1,70 @@
 #!/usr/bin/env python
-from keras.models import load_model
+import cv2  # import opencv
+import keras as keras
 import numpy as np
-import cv2 as cv
 
+np.set_printoptions(suppress=True)
 
-def predict(model, frame):
-    # make predictions with model on frame
-    size = (224, 224)
-    # Create the array of the right shape to feed into the keras model
-    # The 'length' or number of images you can put into the array is
-    # determined by the first position in the shape tuple, in this case 1.
-    data = np.ndarray(shape=(1, *size, 3), dtype=np.float32)
-    # resize the image to a 224x224 with the same strategy as in TM2:
-    # resizing the image to be at least 224x224 and then cropping from the center
-    image = cv.resize(frame, size)
-    # turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
-    # run the inference
-    prediction = model.predict(data)
-    return prediction
+webcam = cv2.VideoCapture(0)
+model = keras.models.load_model('src/keras_model.h5')
+data_for_model = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 
 def load_labels(path):
-    # load labels file from path
-    with open(path) as fp:
-        return dict(map(lambda line: line.split(" ", 1), map(str.strip, fp.readlines())))
+    f = open(path, 'r')
+    lines = f.readlines()
+    labels = []
+    for line in lines:
+        labels.append(line.split(' ')[1].strip('\n'))
+    return labels
 
 
-def main():
-    # Load the model
-    model = load_model("src/keras_model.h5")
-    labels = load_labels("src/labels.txt")
+label_path = 'src/labels.txt'
+labels = load_labels(label_path)
+print(labels)
 
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
+# This function proportionally resizes the image from your webcam to 224 pixels high
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
+
+def image_resize(image, height, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    r = height / float(h)
+    dim = (int(w * r), height)
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
+
+# this function crops to the center of the resize image
+
+
+def cropTo(img):
+    size = 224
+    height, width = img.shape[:2]
+
+    sideCrop = (width - 224) // 2
+    return img[:, sideCrop:(width - sideCrop)]
+
+
+while True:
+    ret, img = webcam.read()
+    if ret:
+        # same as the cropping process in TM2
+        img = image_resize(img, height=224)
+        img = cropTo(img)
+
+        # flips the image
+        img = cv2.flip(img, 1)
+
+        # normalize the image and load it into an array that is the right format for keras
+        normalized_img = (img.astype(np.float32) / 127.0) - 1
+        data_for_model[0] = normalized_img
+
+        # run inference
+        prediction = model.predict(data_for_model)
+        for i in range(0, len(prediction[0])):
+            print('{}: {}'.format(labels[i], prediction[0][i]))
+        cv2.imshow('webcam', img)
+        if cv2.waitKey(1) == 27:
             break
 
-        prediction = predict(model, frame)
-        labelidx = str(np.argmax(prediction[0, :]))
-        labelstr = labels[labelidx]
-        print("pred: {:.2f}, {:.2f}".format(
-            prediction[0, 0], prediction[0, 1]), end="\t")
-        print("labelidx: {}\t labelstr: {}".format(labelidx, labelstr))
-
-        cv.putText(
-            frame,
-            "Label: {}".format(labelstr),  # print text on frame
-            (25, 35), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv.LINE_AA,  # pos
-        )
-
-        # Display the resulting frame
-        cv.imshow("frame", frame)
-        if cv.waitKey(1) == ord("q"):
-            break
-
-    # When everything done, release the capture
-    cap.release()
-    cv.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
+cv2.destroyAllWindows()
